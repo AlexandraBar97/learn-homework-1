522e38eb1cae6b7838f1bbde10500d3d2a8b2c8c
Alexandra Baranova <191984baranova@gmail.com>
2025-06-10 17:54:14 +0300

    шестое задание

--- 6_exception1.py
+++ 6_exception1.py
@@ -1,20 +1,10 @@
-"""
-
-Домашнее задание №1
-
-Исключения: KeyboardInterrupt
-
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
-  и завершала работу при помощи оператора break
-    
-"""
-user = input('Как дела? ')
-def hello_user(user):
-  try:
-    while True:
-      if user == 'Хорошо':
+while True:
+    user = input('Как дела? ')
+    try:
+        if user == 'Хорошо':
+            break
+        else:
+            user
+    except KeyboardInterrupt:
+        print('Пока')
         break
-  except:
-    print('Пока')
-    break
